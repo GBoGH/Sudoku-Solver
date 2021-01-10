73f562c8ab643a8f37e7c133a627601298f0fed9
GBoGH <44493112+GBoGH@users.noreply.github.com>
2021-01-10 11:47:57 +0100

    added GUI, so far, somewhat functional

--- gui.py
+++ gui.py
@@ -1,45 +1,48 @@
 import tkinter as tk
 
-def main():
-    screen_width = 400
-    screen_height = 400
 
-    window = tk.Tk()
-    window.title("Sudoku Solver")
-    canvas = tk.Canvas(width=screen_width, height=screen_height)
-    canvas.pack()
 
-    canvas.create_text(200, 25, text="Enter numbers and press Submit")
+class Draw():
 
-    black = (0,0,0)
+    def __init__(self, root, numbers_input):
+        self.root = root
+        self.grid = []
+        self.numbers_input = numbers_input
+        
+        for i in range(9):
+            self.grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-    x = 87.5
+        for i in range(9):
+            for j in range(9):
+                if (3 <= i <= 5) and (j < 3 or j > 5):
+                    color = "grey"
+                elif (i < 3 or i > 5) and (3 <= j <= 5):
+                    color = "grey"
+                else:
+                    color = "white"
+                self.grid[i][j] = tk.Entry(root, width=2, font=(
+                    "Arial", 20), bg=color, borderwidth=1,
+                    textvar=self.numbers_input[i][j])
+                self.grid[i][j].bind("<FocusOut>", self.correct_input)
+                self.grid[i][j].grid(row=i, column=j)
+
+
+    def correct_input(self, event):
+        for i in range(9):
+            for j in range(9):
+                if len(self.numbers_input[i][j].get()) > 1:
+                    self.numbers_input[i][j].set("")
+                if self.numbers_input[i][j].get() not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                    self.numbers_input[i][j].set("")
+
+def gui():
+    root = tk.Tk()
+    numbers_input = []
     for i in range(9):
-        y = 50
+        numbers_input.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    for i in range(9):
         for j in range(9):
-            entry = tk.Entry()
-            canvas.create_window(x, y, anchor="nw", window=entry, height=20, width=20)
-            y += 25
-        x += 25
-
-    y = 47
-    for i in range(10):
-        if i % 3 == 0 or i == 0:
-            width = 5
-        else:
-            width = 2
-        canvas.create_line(82, y, 312, y, width=width)
-        y += 25
-
-    x = 84
-    for i in range(10):
-        if i % 3 == 0 or i == 0:
-            width = 5
-        else:
-            width = 2
-        canvas.create_line(x, 45, x, 275, width=width)
-        x += 25
-
+            numbers_input[i][j] = tk.StringVar(root)
+    Draw(root, numbers_input)
     tk.mainloop()
-
-main()
---
+++ main.py
@@ -0,0 +1,6 @@
+
+from gui import *
+
+
+
+gui()
